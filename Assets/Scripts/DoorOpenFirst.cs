41fdb07f5cfb7ee76bf64eefb84e6d76e00e4b8c
khang1250 <65101024+khang1250@users.noreply.github.com>
2022-03-03 11:51:47 +0700

    update

---
+++ Assets/Scripts/DoorOpenFirst.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenFirst : MonoBehaviour
+{
+    public GameObject theDoor;
+    public AudioSource doorFX;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        theDoor.GetComponent<Animator>().Play("DoorOpen");
+        doorFX.Play();
+        this.GetComponent<BoxCollider>().enabled = false;
+        StartCoroutine(CloseDoor());
+    }
+
+    IEnumerator CloseDoor()
+    {
+        yield return new WaitForSeconds(2);
+        doorFX.Play();
+        theDoor.GetComponent<Animator>().Play("DoorClose");
+        this.GetComponent<BoxCollider>().enabled = true;
+    }
+
+}
