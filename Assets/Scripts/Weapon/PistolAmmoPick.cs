6bd9ad03350968d42856990b82366ee68780c28b
khang1250 <65101024+khang1250@users.noreply.github.com>
2022-03-24 10:52:09 +0700

    update

---
+++ Assets/Scripts/Weapon/PistolAmmoPick.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolAmmoPick : MonoBehaviour
+{
+    public GameObject fakeAmmoClip;
+    public AudioSource ammoPickUpSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        fakeAmmoClip.SetActive(false);
+        ammoPickUpSound.Play();
+    }
+}
